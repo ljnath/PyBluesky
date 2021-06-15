44608fba1e6674c3c766e54ba310d8c001c96d88
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2021-06-15 16:24:37 +0530

    updated apk icon

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
             'requirements': 'pygame==2.0.0-dev7,aiohttp==3.7.4.post0,multidict==5.1.0,attrs==21.2.0,async-timeout==3.0.1,chardet==4.0.0,idna==3.2,typing-extensions==3.10.0.0,yarl==1.6.3',
             'sdk-dir': '../android-sdk',
             'ndk-dir': '../android-ndk-r19c',
-            'icon': 'assets/images/jet.png',
+            'icon': 'assets/icon/pybluesky.png',
             'dist-name': 'PyBluesky',
             'android-api': 28,
             'bootstrap': 'sdl2',

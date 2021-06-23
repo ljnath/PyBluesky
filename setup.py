e5f48ec0d84cb8de1fc0599310ca921840d388a1
Lakhya Jyoti Nath <lakhya.sci@gmail.com>
2021-06-23 20:25:24 +0530

    made setup.py pep8 complient

--- setup.py
+++ setup.py
@@ -31,29 +31,32 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name = "PyBluesky",
-    version = '1.0.0',
-    author = "Lakhya Jyoti Nath (ljnath)",
-    author_email = 'ljnath@ljnath.com',
-    description = 'A simple python game to navigate your jet and fight though a massive missiles attack based on pygame framework',
-    url = 'https://github.com/ljnath/PyBluesky',
-    packages = find_packages(),
-    package_data = {
+    name="PyBluesky",
+    version='1.0.0',
+    author="Lakhya Jyoti Nath (ljnath)",
+    author_email='ljnath@ljnath.com',
+    description='A simple python game to navigate your jet and fight \
+        though a massive missiles attack based on pygame framework',
+    url='https://github.com/ljnath/PyBluesky',
+    packages=find_packages(),
+    package_data={
         '.': ['main.py'],
         './*': ['*.py'],
         '*/*': ['*.py', '*.ogg', '*.ttf', '*.png', '*.ico'],
         '*/*/*': ['*.py'],
         '*/*/*/*': ['*.py']
         },
-    options = {
+    options={
         'apk': {
             'ignore-setup-py': None,
             'package': 'com.ljnath.pybluesky',
-            'requirements': 'pygame==2.0.0-dev7,aiohttp==3.7.4.post0,multidict==5.1.0,attrs==21.2.0,async-timeout==3.0.1,chardet==4.0.0,idna==3.2,typing-extensions==3.10.0.0,yarl==1.6.3,Plyer',
+            'requirements': 'pygame==2.0.0-dev7,aiohttp==3.7.4.post0,multidict==5.1.0,\
+                attrs==21.2.0,async-timeout==3.0.1,chardet==4.0.0,idna==3.2,\
+                    typing-extensions==3.10.0.0,yarl==1.6.3,Plyer',
             'sdk-dir': '../android-sdk',
             'ndk-dir': '../android-ndk-r19c',
             'presplash':  'assets/images/presplash.png',
-            'presplash-color' : '#C4E2FF',
+            'presplash-color': '#C4E2FF',
             'icon': 'assets/icon/pybluesky.png',
             'dist-name': 'PyBluesky',
             'android-api': 28,
